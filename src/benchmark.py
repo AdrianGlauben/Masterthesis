08b8512b714987b3796909a120ca9a8656b92e5f
AdrianGlauben <adrianphilip.glauben@stud.tu-darmstadt.de>
2022-05-13 13:15:23 +0200

    Finished simple benchmark script

--- src/benchmark.py
+++ src/benchmark.py
@@ -3,15 +3,18 @@ from MCTS_c4 import UCT_search, do_decode_n_move_pieces, get_policy
 from alpha_net_c4 import ConnectNet
 from connect_board import board
 import torch
+import pickle
 
 def play_game(model_1, model_2, expansions_per_move=200, pre_moves=None):
     current_board = board()
+    t = 0.1
     if pre_moves != None:
         for move in pre_moves:
             current_board.drop_piece(move)
+            t = 0.00001
     checkmate = False
     winner = None
-    t = 0.1
+
     while checkmate == False and current_board.actions() != []:
         if current_board.player == 0:
             root = UCT_search(current_board,expansions_per_move,model_1,t)
@@ -40,7 +43,7 @@ def evaluate(model_1, model_2, expansions_per_move=200, use_pre_moves=True):
         with open('./data/eval_positions', 'rb') as pkl_file:
             pre_moves = pickle.load(pkl_file)
 
-        for moves in pre_moves:
+        for i, moves in enumerate(pre_moves):
             winner = play_game(model_1, model_2, expansions_per_move, moves)
             if winner is not None:
                 stats_1[winner] += 1
@@ -48,7 +51,7 @@ def evaluate(model_1, model_2, expansions_per_move=200, use_pre_moves=True):
                 stats_1[2] += 1
             print(f'M1 / M2 ### Games played: {i+1} ### Current stats: {stats_1}')
 
-        for moves in pre_moves:
+        for i, moves in enumerate(pre_moves):
             winner = play_game(model_2, model_1, expansions_per_move, moves)
             if winner is not None:
                 stats_2[abs(winner-1)] += 1
@@ -76,8 +79,8 @@ def evaluate(model_1, model_2, expansions_per_move=200, use_pre_moves=True):
     return stats_1, stats_2
 
 
-MODEL_1_PATH = './training_history/5i_100g_200e_8r/model_data/cc4_current_net__iter4.pth.tar'
-MODEL_2_PATH = './training_history/5i_100g_200e_8r/model_data/cc4_current_net__iter5.pth.tar'
+MODEL_1_PATH = './training_history/run1/cc4_current_net__iter18.pth.tar'
+MODEL_2_PATH = './training_history/5i_w_replay_buffer/model_data/cc4_current_net__iter5.pth.tar'
 
 model_1 = ConnectNet()
 model_2 = ConnectNet()
@@ -92,4 +95,4 @@ if torch.cuda.is_available():
     model_1.cuda()
     model_2.cuda()
 
-print(evaluate(model_1, model_2, 200))
+print(evaluate(model_1, model_2, 200, None))
